092a95a8bba96d303319bcaeb73c8c28b2cc9f7e
victor.ospina <victor.ospina.giraldo@gmail.com>
2020-04-07 20:58:54 +0200

    barra de navegacion

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Pais1Component } from './pais1/pais1.component';
+import { Pais2Component } from './pais2/pais2.component';
+import { Pais3Component } from './pais3/pais3.component';
+
+const routes: Routes = [
+  {
+    path: 'pais1',
+    component:Pais1Component
+  },
+  {
+    path: 'pais2',
+    component:Pais2Component
+  },
+  {
+    path: 'pais3',
+    component:Pais3Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { Pais1Component } from './pais1/pais1.component';
+import { BarraLateralComponent } from './barra-lateral/barra-lateral.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { Pais2Component } from './pais2/pais2.component';
+import { Pais3Component } from './pais3/pais3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ///TopNavBarComponent,
+    Pais1Component,
+    BarraLateralComponent,
+    Pais2Component,
+    Pais3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
